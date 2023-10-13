ecddb3354c4d22e147eb25a054c3140f848e30f9
Perttu Auramo <perttu.auramo@gmail.com>
2023-10-13 12:33:08 +0300

    Store session to DB

--- server/src/sessionInitializer.ts
+++ server/src/sessionInitializer.ts
@@ -1,11 +1,22 @@
 import express, { Express } from 'express'
 import session, { SessionOptions } from 'express-session'
+import genFunc from 'connect-pg-simple'
+import { pool } from './Database'
+
+const PostgresqlStore = genFunc(session)
+const sessionStore = new PostgresqlStore({
+  conString: '<insert-connection-string-here>',
+})
 
 const sessionOptions: SessionOptions = {
   secret: process.env.COOKIE_SECRET!,
   resave: false,
   saveUninitialized: true,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: false },
+  store: new PostgresqlStore({
+    pool: pool,
+    tableName: 'user_sessions',
+  }),
 }
 
 declare module 'express-session' {
